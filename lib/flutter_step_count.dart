fef60730ce6ba8cbacf782e7b963035555849686
张健夫 <451458765@qq.com>
2019-08-06 18:04:06 +0800

    修改返回值类型

--- lib/flutter_step_count.dart
+++ lib/flutter_step_count.dart
@@ -6,8 +6,8 @@ class FlutterStepCount {
   static const MethodChannel _channel =
       const MethodChannel('flutter_step_count');
 
-  static Future<int> get stepCount async {
-    final int stepCount = await _channel.invokeMethod('getStepCount');
+  static Future<String> get stepCount async {
+    final String stepCount = await _channel.invokeMethod('getStepCount');
     return stepCount;
   }
 }
